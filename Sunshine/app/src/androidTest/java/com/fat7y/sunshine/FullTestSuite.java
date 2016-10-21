f32bc0ab64446ba3b5397493e47bd86d98229d15
fathy <muhammed.fat7y@gmail.com>
2016-10-21 10:38:03 +0200

    Sunshine hello world

---
+++ Sunshine/app/src/androidTest/java/com/fat7y/sunshine/FullTestSuite.java
@@ -0,0 +1,20 @@
+package com.fat7y.sunshine;
+
+/**
+ * Created by user on 10/19/2016.
+ */
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
