945981b72f55cdfe91e07be6decf3e48aa30fe3f
fathy <muhammed.fat7y@gmail.com>
2016-11-08 20:06:58 +0200

    Remove
    Remove repeated projects

--- CircusOfPlates/src/eg/edu/alexu/csd/oop/game/state/State.java
+++
@@ -1,9 +0,0 @@
-package eg.edu.alexu.csd.oop.game.state;
-
-
-public interface  State {
-   public void setManager(StateManager manager);
-
-	public void start(String level);
-	public void setScore(int x);
-}
