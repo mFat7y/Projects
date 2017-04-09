b88cdca916546b64a2ffa824c361b7d47632cb4a
fathy <muhammed.fat7y@gmail.com>
2017-04-09 20:41:39 +0200

    Revert "first commit"
    This reverts commit fd8951a6cf35c1487d7be1de7f2ddc9cbe3266de.

--- Sunshine/app/src/main/java/com/fat7y/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/fat7y/sunshine/MainActivity.java
@@ -22,46 +22,46 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.main, menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                startActivity(new Intent(this, SettingsActivity.class));
-                return true;
-            }
-            else if(id == R.id.action_map) {
-                openPreferredLocationMap();
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
+        else if(id == R.id.action_map) {
+            openPreferredLocationMap();
+            return true;
         }
-        private void openPreferredLocationMap() {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-            String location = preferences.getString(getString(R.string.pref_location_key),
-                              getString(R.string.pref_location_default));
+        return super.onOptionsItemSelected(item);
+    }
+    private void openPreferredLocationMap() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = preferences.getString(getString(R.string.pref_location_key),
+                          getString(R.string.pref_location_default));
 
-            Uri locationGeo = Uri.parse("geo:0,0?").buildUpon().
-                              appendQueryParameter("q", location).build();
+        Uri locationGeo = Uri.parse("geo:0,0?").buildUpon().
+                          appendQueryParameter("q", location).build();
 
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(locationGeo);
-            if(intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            }
-            else {
-                Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
-            }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(locationGeo);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
         }
+        else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+    }
 }
